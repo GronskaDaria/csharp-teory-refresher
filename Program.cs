e13ccdec83fb1fcec723ccf4fdc0c37582af7351
Daria Gronska <gronskaadariaa@gmail.com>
2026-01-28 00:34:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyProject
+{
+    internal class Program
+    {
+        //Entery point of the application
+        static void Main(string [] args)
+        //Body of the Main method
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Beep();
+
+        }
+
+    }
+}
