a16b7c0624b63928a7842683a1c4815f1ef9479e
Daria Gronska <gronskaadariaa@gmail.com>
2026-01-28 06:47:55 +0100

    add example using of params keyword

--- Program.cs
+++ Program.cs
@@ -8,21 +8,25 @@ namespace MyProject
         static void Main(string [] args)
         //Body of the Main method
         {
-            //Overloading
-            Console.WriteLine(Multiply(2.3, 45.1));
-            Console.WriteLine(Multiply(2.3, 45.1, 1.5));
+            //params keyword allows passing a variable number of arguments to a method
+            //The method can accept zero or more arguments of a specified type
+
+            Console.WriteLine("Total price: "+ CheckOut(1,4,5,7,23));
+
 
             Console.ReadKey();
         }
 
-        static double Multiply(double a, double b)
+        static double CheckOut(params double [] prices)
         {
-            return a * b;
+            double total = 0;
+            foreach (double price in prices)
+            {
+                total += price;
+            }
+            return total;
         }
 
-        static double Multiply(double a, double b, double c)
-        {
-            return a * b * c;
-        }
+
     }
 }
