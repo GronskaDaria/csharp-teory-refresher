57e3b811872636261744e2e929f43d3497ff03ec
Daria Gronska <gronskaadariaa@gmail.com>
2026-01-28 02:56:44 +0100

    add example of user input

--- Program.cs
+++ Program.cs
@@ -8,30 +8,14 @@ namespace MyProject
         static void Main(string [] args)
         //Body of the Main method
         {
-            double a = 5.35;
-            int b = Convert.ToInt32(a);
+            Console.WriteLine("Enter your name:");
+            //Read user input from console
+            string name =Console.ReadLine();
 
-            int c = 12;
-            double d= Convert.ToDouble(c);
+            Console.WriteLine("Enter your age:");
+            int age =int.Parse(Console.ReadLine());
 
-            int e = 100;
-            string f = Convert.ToString(e);
-
-            string g = "256";
-            int h = Convert.ToInt32(g);
-
-            string i = "#";
-            char j = Convert.ToChar(i);
-
-            string k = "true";
-            bool l = Convert.ToBoolean(k);
-
-            // Implicit casting (automatically done by the compiler)
-            int intVar = 10;
-            double doubleVar = intVar+0.1;
-
-            Console.WriteLine(doubleVar);
-            Console.WriteLine(doubleVar.GetType());
+            Console.WriteLine("Hello, " + name.ToUpper() + "! \n You was born in " + (DateTime.Now.Year-age));
             Console.ReadKey();
         }
     }
