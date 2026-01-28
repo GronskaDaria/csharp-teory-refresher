66708d1cf7d77a46c7c78d5560c47d965d620624
Daria Gronska <gronskaadariaa@gmail.com>
2026-01-28 01:20:19 +0100

    add information about Variables

--- CheatSheet.cs
+++ CheatSheet.cs
@@ -10,3 +10,30 @@
 // \' - Single quote
 // \" - Double quote
 // \\ - Backslash
+
+//Variables and Data Types
+// Declaring an integer variable, stored numbers in 4 bytes
+//int integerVar = 42;
+
+// Declaring a string variable, stored text data in multiple bytes
+//string stringVar = "Hello, World!";
+
+// Declaring a boolean variable, stored true/false values in 1 byte
+//bool boolVar = true;
+
+// Declaring a floating-point variable, stored numbers with decimals in 4 bytes
+//float floatVar = 3.14f;
+
+// Declaring a double variable, stored numbers with decimals in 8 bytes
+//double doubleVar = 3.14159;
+
+// Declaring a character variable, stored single 16-bit Unicode characters in 2 bytes
+//char charVar = 'A';
+
+// Declaring a decimal variable, stored high-precision decimal numbers in 16 bytes
+//decimal decimalVar = 19.99m;
+
+// Declaring a constant variable, value cannot be changed after initialization
+//const double Pi = 3.14159;
+
+
--- Program.cs
+++ Program.cs
@@ -8,13 +8,23 @@ namespace MyProject
         static void Main(string [] args)
         //Body of the Main method
         {
-            Console.WriteLine("Hello, World!");
-            // Wait for user input before closing
+            int x; // Variable declaration
+            x = 10; // Variable initialization
+            int y = 20; // Combined declaration and initialization 
+
+
+            int age = 25;
+            double height = 1.9;
+            string name = "John Doe";
+            bool isStudent = true;
+            char grade = 'A';
+
+            Console.WriteLine("My name is "+name);
+            Console.WriteLine("My age is "+age +" cm");
+            Console.WriteLine("My height "+height);
+            Console.WriteLine("Am I student? "+ isStudent);
+            Console.WriteLine("My test grade is "+ grade);
             Console.ReadKey();
-            /// End of Main method
-            /// 
-
         }
-
     }
 }
